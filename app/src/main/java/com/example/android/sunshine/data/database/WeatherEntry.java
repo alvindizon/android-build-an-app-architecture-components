5643f6b784fd6913dd4f8e1559293085716b7dd9
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-10 14:52:54 +0800

    Add Room to project, create an Entity from WeatherEntry
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
@@ -16,10 +16,24 @@
 
 package com.example.android.sunshine.data.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Date;
 
+/**
+ * Creates a table called "weather" out of this class. The date is used an index
+ * so that its uniqueness is ensured
+ */
+@Entity(tableName = "weather", indices = {@Index(value = {"date"}, unique = true)})
 public class WeatherEntry {
-
+    /**
+     * makes id the primary key,
+     * the autoGenerate attrib is set to true so that Room generates the key
+     */
+    @PrimaryKey(autoGenerate = true)
     private int id;
     private int weatherIconId;
     private Date date;
@@ -42,7 +56,21 @@ public class WeatherEntry {
      * @param wind Wind speed
      * @param degrees Wind direction
      */
+    @Ignore
     public WeatherEntry(int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
+        this.weatherIconId = weatherIconId;
+        this.date = date;
+        this.min = min;
+        this.max = max;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.wind = wind;
+        this.degrees = degrees;
+    }
+
+//    This constructor will be used by Room.
+    public WeatherEntry(int id, int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
+        this.id = id;
         this.weatherIconId = weatherIconId;
         this.date = date;
         this.min = min;
