147a72eda8882472fd5072f4781539d4ceda90cb
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-11 14:54:40 +0800

    Add view model for MainActivity
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

---
+++ app/src/main/java/com/example/android/sunshine/ui/list/MainActivityViewModel.java
@@ -0,0 +1,27 @@
+package com.example.android.sunshine.ui.list;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.android.sunshine.data.SunshineRepository;
+import com.example.android.sunshine.data.database.WeatherEntry;
+
+import java.util.List;
+
+/**
+ * {@link ViewModel} for {@link MainActivity}
+ */
+public class MainActivityViewModel extends ViewModel {
+
+    private LiveData<List<WeatherEntry>> mWeatherEntryList;
+    private final SunshineRepository mRepository;
+
+    public MainActivityViewModel(SunshineRepository mRepository) {
+        this.mRepository = mRepository;
+        mWeatherEntryList = mRepository.getLatestWeatherForecasts();
+    }
+
+    public LiveData<List<WeatherEntry>> getmWeatherEntryList(){
+        return mWeatherEntryList;
+    }
+}
