5288569b5c945cbf0ca8d288b9db5444062eb84a
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-11 15:29:19 +0800

    Setup ViewModel and factory in MainActivity
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/example/android/sunshine/ui/list/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/ui/list/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.sunshine.ui.list;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +26,7 @@ import android.widget.ProgressBar;
 
 import com.example.android.sunshine.R;
 import com.example.android.sunshine.ui.detail.DetailActivity;
+import com.example.android.sunshine.utilities.InjectorUtils;
 
 import java.util.Date;
 
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements
     private RecyclerView mRecyclerView;
     private int mPosition = RecyclerView.NO_POSITION;
     private ProgressBar mLoadingIndicator;
+    private MainActivityViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,6 +105,9 @@ public class MainActivity extends AppCompatActivity implements
         mRecyclerView.setAdapter(mForecastAdapter);
         showLoading();
 
+        MainViewModelFactory factory = InjectorUtils.provideMainActivityViewModelFactory(
+                this.getApplicationContext());
+        mViewModel = ViewModelProviders.of(this, factory).get(MainActivityViewModel.class);
     }
 
     /**
